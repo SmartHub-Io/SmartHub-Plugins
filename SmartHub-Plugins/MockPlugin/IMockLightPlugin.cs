cae0921e3de177fdfca7ed7ca7c9baffe08563bd
Maximilian Stümpfl <maximilian2304@t-online.de>
2020-06-05 20:54:27 +0200

    add the firstplugins
    to a new solution and link to the shared plugin form smarthub baseplugin

---
+++ SmartHub-Plugins/MockPlugin/IMockLightPlugin.cs
@@ -0,0 +1,10 @@
+﻿using SmartHub.BasePlugin.Interfaces;
+using SmartHub.BasePlugin.Interfaces.DeviceType;
+
+namespace MockPlugin
+{
+	public interface IMockLightPlugin : IHttpSupport, ILight
+	{
+
+	}
+}
